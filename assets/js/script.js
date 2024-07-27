9b9dffd15071ca7e33766633b313f412d97581fb
elmeddinx <xudaverdiyev11@gmail.com>
2024-07-27 11:08:15 +0400

    Change js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 function isMobileDevice() {
     return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 }
-var swiper = new Swiper(".heroSwiper", {
-    direction: "vertical",
+var heroSwiperConfig = {
+    direction: isMobileDevice() ? 'horizontal' : 'vertical',
     mousewheel: {
         releaseOnEdges: true,
     },
@@ -15,51 +15,12 @@ var swiper = new Swiper(".heroSwiper", {
         el: ".swiper-pagination",
         clickable: true,
     },
-    on: {
-        reachEnd: function() {
-          window.isAtEnd = true;
-        },
-        slideChange: function() {
-          window.isAtEnd = false;
-        }
-      }
-    // touchRatio: 0,
-    // simulateTouch: false,
-});
+};
+var swiper = new Swiper('.heroSwiper', heroSwiperConfig);
 
-document.addEventListener('wheel', function(event) {
-    if (window.isAtEnd) {
-      event.preventDefault(); 
-      if (event.deltaY > 0) {
-        window.scrollBy({
-          top: window.innerHeight,
-          behavior: 'smooth'
-        });
-      } else {
-        window.scrollBy({
-          top: -window.innerHeight,
-          behavior: 'smooth'
-        });
-      }
-    }
-  }, { passive: false });
-
-  let startY = 0;
-  document.addEventListener('touchstart', function(event) {
-    if (window.isAtEnd) {
-      startY = event.touches[0].clientY;
-    }
-  });
-
-  document.addEventListener('touchend', function(event) {
-    if (window.isAtEnd) {
-      let endY = event.changedTouches[0].clientY;
-      let deltaY = endY - startY;
-      if (Math.abs(deltaY) > 50) {
-        window.scrollBy({
-          top: deltaY > 0 ? window.innerHeight : -window.innerHeight,
-          behavior: 'smooth'
-        });
-      }
-    }
-  });
+if (isMobileDevice()) {
+    swiper.allowTouchMove = true;
+    document.querySelector('.swiper-container').addEventListener('touchstart', function (e) {
+        e.stopPropagation();
+    }, { passive: true });
+}
